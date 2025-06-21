58b95b8e2134a5abf8e3ce2833ca389c445a8d05
Inder Dhir <inderdeep@gmail.com>
2025-06-20 18:37:59 -0700

    ignore messages that are not parsed

--- destinations/log.py
+++ destinations/log.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import json
 import csv
@@ -8,9 +9,15 @@ STDOUT_CSV_WRITER = csv.writer(sys.stdout, quoting=csv.QUOTE_MINIMAL)
 class ConsoleDestinationHandler(DestinationHandler):
     @staticmethod
     def push(log:dict):
+        if not isinstance(log, dict) or len(dict) < 1:
+            logging.debug("Unexpected type=%s (or empty dict)", type(log))
+            return
         print(json.dumps(log))
 
 class CSVHandler(DestinationHandler):
     @staticmethod
     def push(log:dict):
+        if not isinstance(log, dict) or len(dict) < 1:
+            logging.debug("Unexpected type=%s (or empty dict)", type(log))
+            return
         STDOUT_CSV_WRITER.writerow(log.values())
